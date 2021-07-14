bdafde17f444abc100d90d1e16d5b2b6ad7bc039
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-07-15 00:17:17 +0900

    MycobotBasic(0fe6ca5)ベース

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,12 @@
-#include "main.h"
+#include <MycobotBasic.h>
+#include <ParameterList.h>
 
-const bool ENABLE_SERIAL = true;
-
-#if defined(TARGET_M5STACK)
-const bool ENABLE_LCD = true;
-const bool ENABLE_SD = true;
-const bool ENABLE_I2C = false;
-#define M5_BEGIN M5.begin(ENABLE_LCD, ENABLE_SD, ENABLE_SERIAL, ENABLE_I2C)
-#elif defined(TARGET_M5STICKC)
-const bool ENABLE_LCD = true;
-const bool ENABLE_POWER = true;
-#define M5_BEGIN M5.begin(ENABLE_LCD, ENABLE_POWER, ENABLE_SERIAL)
-#elif defined(TARGET_M5ATOM)
-const bool ENABLE_I2C = false;
-const bool ENABLE_DISPLAY = true;
-#define M5_BEGIN M5.begin(ENABLE_SERIAL, ENABLE_I2C, ENABLE_DISPLAY)
-#elif defined(TARGET_M5PAPER)
-const bool ENABLE_TOUCH = true;
-const bool ENABLE_SD = true;
-const bool ENABLE_BATTERY_ADC = true;
-const bool ENABLE_I2C = false;
-#define M5_BEGIN M5.begin(ENABLE_TOUCH, ENABLE_SD, ENABLE_SERIAL, ENABLE_BATTERY_ADC, ENABLE_I2C)
-#endif
+MycobotBasic myCobot;
 
 void setup(void)
 {
-    M5_BEGIN;
+    myCobot.setup();
+    myCobot.powerOn();
 }
 
 void loop(void)
