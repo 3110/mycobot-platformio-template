2f3155f7e40b27d09bb366d7dde07f24ec1d24c2
SAITO, Tetsuya <saito.tetsuya@gmail.com>
2021-08-31 20:29:56 +0900

    フォーマッタの適用

--- lib/MycobotBasic/MycobotSaver.h
+++ lib/MycobotBasic/MycobotSaver.h
@@ -2,35 +2,29 @@
 #define mycobotsaver_h
 
 #include <Arduino.h>
+
 #include "FS.h"
 #include "SPIFFS.h"
 #define FORMAT_SPIFFS_IF_FAILED true
 
 #define FILENAME "/Angles.txt"
 
-
-class MycobotSaver
-{
+class MycobotSaver {
 public:
+    MycobotSaver();
 
-	MycobotSaver();
+    typedef struct {
+        int joint_angle[6];
+    } saver_angles_enc;
 
-	typedef struct{
-	  int joint_angle[6];
-	} saver_angles_enc;
-
-	void listDir(fs::FS &fs, const char * dirname, uint8_t levels);
-	void writeFile(fs::FS &fs, const char * path, const char * message);
-	void appendFile(fs::FS &fs, const char * path, const char * message);
-	void deleteFile(fs::FS &fs, const char * path);
-	void readFile(fs::FS &fs, const char * path);
-	saver_angles_enc processStringIntoInts(String string_input);
-
+    void listDir(fs::FS &fs, const char *dirname, uint8_t levels);
+    void writeFile(fs::FS &fs, const char *path, const char *message);
+    void appendFile(fs::FS &fs, const char *path, const char *message);
+    void deleteFile(fs::FS &fs, const char *path);
+    void readFile(fs::FS &fs, const char *path);
+    saver_angles_enc processStringIntoInts(String string_input);
 
 private:
-
-
 };
-
 
 #endif
